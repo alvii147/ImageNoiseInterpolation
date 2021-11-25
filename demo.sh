79c1158fc2e47de16d4afe506e9222dc584b9160
Zahin <alvii147@gmail.com>
2021-11-26 01:19:22 +0600

    added echo commands to show status

--- demo.sh
+++ demo.sh
@@ -5,6 +5,9 @@ NOISY_IMG=img/birb_noisy.png
 INTERPOLATED_IMG=img/birb_interpolated.png
 MEDIAN_IMG=img/birb_median.png
 
+echo Adding noise to $ORIGINAL_IMG
 python noisify.py $ORIGINAL_IMG $NOISY_IMG -p 0.5
+echo Applying interpolation on $NOISY_IMG
 python interpolate.py $NOISY_IMG $INTERPOLATED_IMG -E 15
+echo Applying median filter on $NOISY_IMG
 python median.py $NOISY_IMG $MEDIAN_IMG -N 3
